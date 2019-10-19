416c689459e7c1507e8701d3e4325de0a1463598
Erich Styger <erich.styger@hslu.ch>
2019-10-19 06:24:08 +0200

    cleanup

--- Generator/readme.txt
+++
@@ -1,3 +0,0 @@
-readme.txt
-----------
-This folder contains the Processor Expert project(s) to generate the library files.
--- lib/config/McuTimeDateconfig.h
+++
@@ -1,6 +0,0 @@
-#ifndef __McuTimeDate_CONFIG_H
-#define __McuTimeDate_CONFIG_H
-
-/* no configuration supported yet */
-
-#endif /* __McuTimeDate_CONFIG_H */
