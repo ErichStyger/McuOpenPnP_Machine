79743960e23ec6a8b28d6ea2b3dc91f2e1bd1d3a
Erich Styger <erich.styger@hslu.ch>
2020-03-20 07:58:39 +0100

    added generated code to the repository

---
+++ Projects/tinyK20_SolderDispenser/Generated_Code/derivative.h
@@ -0,0 +1,15 @@
+#ifndef __DERIVATIVE_USB
+#define __DERIVATIVE_USB
+
+/* Include the derivative-specific header file */
+#if 0 /* << EST */
+  #include <MK20D5.h>
+#else
+  #include "Cpu.h"
+#endif
+
+#define USED_PIT0
+#define __MK_xxx_H__
+
+#endif
+
---
+++ Projects/tinyK22_OpenPnP_Master/Generated_Code/CDC1config.h
@@ -0,0 +1,20 @@
+#ifndef __CDC1_CONFIG_H
+#define __CDC1_CONFIG_H
+
+#ifndef CDC1_CONFIG_USE_TIMEOUT
+  #define CDC1_CONFIG_USE_TIMEOUT           1
+    /*!< 1: Use timeout; 0: do not use timeout */
+#endif
+
+#ifndef CDC1_CONFIG_APP_TASK_TIMEOUT_MS
+  #define CDC1_CONFIG_APP_TASK_TIMEOUT_MS   20
+    /*!< App Task timeout in milliseconds, 0 to disable timout */
+#endif
+
+#ifndef CDC1_CONFIG_DATA_BUF_SIZE
+  #define CDC1_CONFIG_DATA_BUF_SIZE           (64)
+    /*!< Value used for CDC1_DATA_BUF_SIZE, must be a multiple of endpoint size. The greater the value, the better the performance */
+#endif
+
+
+#endif /* __CDC1_CONFIG_H */
