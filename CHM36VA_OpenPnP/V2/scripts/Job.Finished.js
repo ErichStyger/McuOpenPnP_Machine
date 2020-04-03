27e307bfe9e576e0743cd3f70f294a3cd67871b7
Erich Styger <erich.styger@hslu.ch>
2020-04-03 12:40:00 +0200

    updated finished script file

--- CHM36VA_OpenPnP/V2/scripts/Job.Finished.js
+++ CHM36VA_OpenPnP/V2/scripts/Job.Finished.js
@@ -8,3 +8,17 @@
    vac.actuate(false);  /* VAC off */
  }
  print('*************** END Job.Finished.js ****************');
+
+
+// Job.Finished.js
+//
+// resets all parts to not placed
+
+//print("### JOB FINISHED ###");
+
+//var n = 0;
+//for each (var boardlocation in job.getBoardLocations()) {
+//  boardlocation.clearAllPlaced();
+//  n++;
+//}
+//print(n + " Board(s) reset.");
